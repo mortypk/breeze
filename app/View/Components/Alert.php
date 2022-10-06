183e23a9681f7a316d2a50724bd2acdd6c14c0e8
mortypk <motypk@gmail.com>
2022-10-06 15:40:51 +0500

    alter components add

---
+++ app/View/Components/Alert.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class Alert extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public $title, $type='info', $typeColor='blue';
+
+    public function __construct($type)
+    {
+        if($type == 'success'){
+            $this->typeColor='green';
+        }
+        elseif($type == 'warning'){
+            $this->typeColor='yellow';
+        }
+        elseif($type == 'danger'){
+            $this->typeColor='red';
+        }
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.alert');
+    }
+}
